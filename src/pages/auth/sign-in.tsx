862306b8fddbc6b4475f9a046dfa48c1f7ee4d44
Jose Acevedo <josearmandoacevedoangarita@gmail.com>
2023-06-13 22:16:32 -0500

    Primer commit tengo ganas de dormir

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -168,12 +168,12 @@ export default function SignIn() {
                     Forgot password?
                   </Link>
                 </div>
-                <button
-                  type="submit"
+                <Link
+                  href={'/'}
                   className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   Sign in to your account
-                </button>
+                </Link>
               </form>
             </div>
           </div>
